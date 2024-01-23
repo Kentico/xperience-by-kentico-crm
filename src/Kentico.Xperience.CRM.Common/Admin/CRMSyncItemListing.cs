e73f82568945fdda9edeba1767278c57cf8a4037
Martin Foukal <foukal@bluesoft.cz>
2024-01-23 13:40:42 +0100

    Sync item listing typo fixes, email column, client secret as password component

--- src/Kentico.Xperience.CRM.Common/Admin/CRMSyncItemListing.cs
+++ src/Kentico.Xperience.CRM.Common/Admin/CRMSyncItemListing.cs
@@ -1,5 +1,8 @@
 ﻿using CMS.DataEngine;
+using CMS.FormEngine;
+using CMS.Helpers;
 using CMS.OnlineForms;
+using CMS.OnlineForms.Internal;
 using Kentico.Xperience.Admin.Base;
 using Kentico.Xperience.Admin.DigitalMarketing.UIPages;
 using Kentico.Xperience.CRM.Common.Admin;
@@ -7,7 +10,7 @@ using Kentico.Xperience.CRM.Common.Classes;
 
 [assembly:
     UIPage(parentType: typeof(FormEditSection), slug: "crm-sync-listing", uiPageType: typeof(CRMSyncItemListing),
-        name: "CRM synchronization", templateName: TemplateNames.LISTING, order: 1000, icon: Icons.IntegrationScheme)]
+        name: "CRM sync", templateName: TemplateNames.LISTING, order: 1000, icon: Icons.IntegrationScheme)]
 
 namespace Kentico.Xperience.CRM.Common.Admin;
 
@@ -16,6 +19,7 @@ namespace Kentico.Xperience.CRM.Common.Admin;
 /// </summary>
 internal class CRMSyncItemListing : ListingPage
 {
+    private readonly IContactFieldFromFormRetriever contactFieldFromFormRetriever;
     private BizFormInfo? editedForm;
     private DataClassInfo? dataClassInfo;
     protected override string ObjectType => CRMSyncItemInfo.OBJECT_TYPE;
@@ -23,20 +27,45 @@ internal class CRMSyncItemListing : ListingPage
     /// <summary>ID of the edited form.</summary>
     [PageParameter(typeof(IntPageModelBinder), typeof(FormEditSection))]
     public int FormId { get; set; }
-    
+
     private BizFormInfo EditedForm =>
         this.editedForm ??= AbstractInfo<BizFormInfo, IBizFormInfoProvider>.Provider.Get(FormId);
 
     private DataClassInfo DataClassInfo => this.dataClassInfo ??=
         DataClassInfoProviderBase<DataClassInfoProvider>.GetDataClassInfo(EditedForm.FormClassID);
 
+    public CRMSyncItemListing(IContactFieldFromFormRetriever contactFieldFromFormRetriever)
+    {
+        this.contactFieldFromFormRetriever = contactFieldFromFormRetriever;
+    }
+
     public override Task ConfigurePage()
     {
+        var primaryColumnName = new BizFormItem(DataClassInfo.ClassName).TypeInfo.IDColumn;
+
         PageConfiguration.ColumnConfigurations
-            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityID), "Form item ID")
-            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityCRM), "CRM")
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityID), formatter: (value, _) =>
+            {
+                var bizFormItem =
+                    BizFormItemProvider.GetItem(ValidationHelper.GetInteger(value, 0), DataClassInfo.ClassName);
+                // first try to get email by mapping to Contact email
+                var email = contactFieldFromFormRetriever.RetrieveContactEmail(bizFormItem);
+                if (email is not null)
+                {
+                    return email;
+                }
+
+                //secondary try to find email field from field with 'Email' in his name
+                var emailField = new FormInfo(DataClassInfo.ClassFormDefinition).ItemsList.OfType<FormFieldInfo>()
+                    .FirstOrDefault(fi =>
+                        fi.Name.Contains("email", StringComparison.InvariantCultureIgnoreCase) && fi.DataType == "text")
+                    ?.Name;
+                return bizFormItem.GetStringValue(emailField, string.Empty);
+            }, caption: "Email")
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityID), "Form item ID", maxWidth: 20)
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityCRM), "CRM", maxWidth: 20)
             .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemCRMID), "CRM ID")
-            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemLastModified), "Last sync time");
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemLastModified), "Last sync");
 
         PageConfiguration.QueryModifiers.AddModifier(q =>
             q.WhereEquals(nameof(CRMSyncItemInfo.CRMSyncItemEntityClass), DataClassInfo.ClassName)
--- src/Kentico.Xperience.CRM.Common/Models/CRMIntegrationSettingsModel.cs
+++ src/Kentico.Xperience.CRM.Common/Models/CRMIntegrationSettingsModel.cs
@@ -9,23 +9,24 @@ public class CRMIntegrationSettingsModel
 {
     [CheckBoxComponent(Label = "Forms enabled", Order = 1)]
     public bool FormsEnabled { get; set; }
-    
+
     [CheckBoxComponent(Label = "Contacts enabled", Order = 2)]
     public bool ContactsEnabled { get; set; }
-    
+
     [CheckBoxComponent(Label = "Ignore existing records", Order = 3)]
     public bool IgnoreExistingRecords { get; set; }
-    
+
     [UrlValidationRule]
     [TextInputComponent(Label = "CRM URL", Order = 4)]
     [RequiredValidationRule]
     public string? Url { get; set; }
-    
+
     [RequiredValidationRule]
     [TextInputComponent(Label = "Client ID", Order = 5)]
     public string? ClientId { get; set; }
-    
+
     [RequiredValidationRule]
-    [TextInputComponent(Label = "Client secret", Order = 6)]
+    [PasswordComponent(Label = "Client secret", Order = 6, RequiredLength = 0, RequireDigit = false,
+        RequireLowercase = false, RequireUppercase = false, RequiredUniqueChars = 0, RequireNonAlphanumeric = false)]
     public string? ClientSecret { get; set; }
 }
