41ee5635f412a7470535df9381347dd61d52db09
Martin Foukal <foukal@bluesoft.cz>
2024-02-06 13:34:16 +0100

    listing page for synced contacts

---
+++ src/Kentico.Xperience.CRM.Common/Admin/CRMContactSyncItemListing.cs
@@ -0,0 +1,35 @@
+﻿using Kentico.Xperience.Admin.Base;
+using Kentico.Xperience.Admin.DigitalMarketing.UIPages;
+using Kentico.Xperience.CRM.Common.Admin;
+using CMS.ContactManagement;
+
+[assembly: UIPage(
+    parentType: typeof(ContactManagementApplication),
+    slug: "crm-contact-sync-listing",
+    uiPageType: typeof(CRMContactSyncItemListing),
+    name: "CRM contacts sync",
+    templateName: TemplateNames.LISTING,
+    order: 1000,
+    icon: Icons.IntegrationScheme)]
+
+namespace Kentico.Xperience.CRM.Common.Admin;
+
+internal class CRMContactSyncItemListing : ListingPage
+{
+    protected override string ObjectType => CRMSyncItemInfo.OBJECT_TYPE;
+
+    public override Task ConfigurePage()
+    {
+        PageConfiguration.ColumnConfigurations            
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityID), "Email", maxWidth: 50)
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemEntityCRM), "CRM", maxWidth: 20)
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemCRMID), "CRM ID")
+            .AddColumn(nameof(CRMSyncItemInfo.CRMSyncItemLastModified), "Last sync");
+
+        PageConfiguration.QueryModifiers.AddModifier(q =>
+            q.WhereEquals(nameof(CRMSyncItemInfo.CRMSyncItemEntityClass), ContactInfo.TYPEINFO.ObjectClassName)
+                .OrderByDescending(nameof(CRMSyncItemInfo.CRMSyncItemLastModified)));
+
+        return base.ConfigurePage();
+    }
+}
